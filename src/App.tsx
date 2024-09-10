e986c17d581105c0dab166917c9a3b33dd03b678
Amit <singhaks1527@gmail.com>
2024-09-10 15:30:33 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -26,30 +26,34 @@ const App = () => {
     <Router>
       <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path="/" element={<Link to="/admin/dashboard"><button className="app">Visit Dashboard</button></Link>}></Route>
-            <Route path="/admin/dashboard" element={<Dashboard />} />
-            <Route path="/admin/products" element={<Products />} />
-            <Route path="/admin/transaction" element={<Transaction />} />
-            <Route path="/admin/customers" element={<Customers />} />
+          <Route 
+            path="/" 
+            element={
+              <Link to="/admin/dashboard"><button className="app">Visit Dashboard</button></Link>
+            }> 
+          </Route>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/products" element={<Products />} />
+          <Route path="/admin/transaction" element={<Transaction />} />
+          <Route path="/admin/customers" element={<Customers />} />
 
 
-            {/* Charts */}
-            <Route path="/admin/chart/bar" element={<BarCharts />} />
-            <Route path="/admin/chart/line" element={<LineCharts />} />
-            <Route path="/admin/chart/pie" element={<PieCharts />} />
+          {/* Charts */}
+          <Route path="/admin/chart/bar" element={<BarCharts />} />
+          <Route path="/admin/chart/line" element={<LineCharts />} />
+          <Route path="/admin/chart/pie" element={<PieCharts />} />
 
 
-            {/* Apps */}
-            <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
-            <Route path="/admin/app/coupon" element={<Coupon />} />
-            <Route path="/admin/app/toss" element={<Toss />} />
+          {/* Apps */}
+          <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+          <Route path="/admin/app/coupon" element={<Coupon />} />
+          <Route path="/admin/app/toss" element={<Toss />} />
 
 
-            {/* Management */}
-            <Route path="/admin/products/new" element={<NewProduct />} />
-            <Route path="/admin/products/:id" element={<ProductManagement />} />
-            <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
-          
+          {/* Management */}
+          <Route path="/admin/products/new" element={<NewProduct />} />
+          <Route path="/admin/products/:id" element={<ProductManagement />} />
+          <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
         </Routes>
       </Suspense>
     </Router>
